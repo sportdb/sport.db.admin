a37c9526c2af6bd5506959c3a43f43b634e0572d
Gerald Bauer <gerald.bauer@gmail.com>
2012-10-07 19:01:41 +0200

    polish views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
-end
+
+  def powered_by
+    ## todo/fix: use version from wettpool module
+    content_tag :div do
+      link_to( 'sport.db.admin/1', 'https://github.com/geraldb/sport.db.admin' )  + ', ' +
+      link_to( "sport.db/#{SportDB::VERSION}", 'https://github.com/geraldb/sport.db' ) + ' - ' +
+      content_tag( :span, "Ruby/#{RUBY_VERSION} (#{RUBY_RELEASE_DATE}/#{RUBY_PLATFORM}) on") + ' ' +
+      content_tag( :span, "Rails/#{Rails.version} (#{Rails.env})" )
+      ## content_tag( :span, "#{request.headers['SERVER_SOFTWARE'] || request.headers['SERVER']}" )
+    end
+  end
+
+end # module ApplicationHelper
--- app/models/game.rb
+++ app/models/game.rb
@@ -118,6 +118,26 @@ class Game < ActiveRecord::Base
       self.toto12x = '2'
     end
   end
+  
+  
+  def score_str
+    if score5.present? && score6.present?    # im Elfmeterschiessen i.E.?
+      "#{score5} : #{score6} i.E. / #{score3} : #{score4} n.V. / #{score1_str} : #{score2_str}"
+    elsif score3.present? && score4.present?  # nach Verlaengerung n.V.?
+       "#{score3} : #{score4} n.V. / #{score1_str} : #{score2_str}"
+    else
+      "#{score1_str} : #{score2_str}"
+    end
+  end
+
+  def score1_str
+    if score1.blank? then '-' else score1.to_s end
+  end
+
+  def score2_str
+    if score2.blank? then '-' else score2.to_s end
+  end
+  
 
 end # class Game
 
