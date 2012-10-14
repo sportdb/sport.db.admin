0fe24c6ab093a4ab105eebb76b8e4973f95ef582
Gerald Bauer <gerald.bauer@gmail.com>
2012-10-14 23:46:32 +0200

    polish views for teams, rounds, etc.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,11 @@ class TeamsController < ApplicationController
 
   # GET /teams
   def index
-    @teams = Team.all
+    
+    # todo: join country table to order by country.key
+    
+    @national_teams = Team.where( national: true ).order( 'country_id' ).all
+    @club_teams     = Team.where( club: true ).order( 'country_id' ).all
   end
 
   # GET /teams/1
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
     end
   end
 
+  ### todo/fix: move to format helper
+  
+  def fmt_date_with_week( date )
+    if date.nil?
+      '-'
+    else
+      ##  e.g. 2012 (Week 22) // Oct/21 Fri
+      date.strftime( "%Y (Week %V) // %b/%d %a" )
+    end
+  end
+  
+
 end # module ApplicationHelper
