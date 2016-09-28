7e62082e0e999fd37afb1e5f310aeabdbe0c571d
Gerald Bauer <gerald.bauer@gmail.com>
2016-09-28 08:34:00 +0200

    up rails 4.2.x

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,7 @@
-require 'rubygems'
 
-# Set up gems listed in the Gemfile.
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 puts "[boot] boot.rb - before require 'bundler/setup'"
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 puts "[boot] boot.rb - after require 'bundler/setup'"
-
