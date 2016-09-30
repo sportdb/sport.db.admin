b630ec403e3a32f6e808669846ed1d0461878442
Gerald Bauer <gerald.bauer@gmail.com>
2016-09-30 09:27:43 +0200

    up assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Sportdbhost::Application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Sportdbhost::Application.config.assets.precompile += %w(*.png)
